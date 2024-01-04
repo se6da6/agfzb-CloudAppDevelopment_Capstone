2d6e25886ca4e8b3317be6197024b6f418e5d954
se6da6 <107140027+se6da6@users.noreply.github.com>
2024-01-04 15:04:35 -0500

    design admin.py

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,31 @@
 from django.contrib import admin
 # from .models import related models
-
+from .models import CarMake, CarModel
 
 # Register your models here.
 
 # CarModelInline class
+class CarModelInline(admin.TabularInline):
+    model = CarModel
+    extra = 1  # Number of empty CarModel forms to display
 
 # CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ['name', 'car_make', 'dealer_id', 'type', 'year']
+    list_filter = ['car_make', 'type', 'year']
+    search_fields = ['name', 'car_make__name']
+
+    # Customize other admin options as needed
 
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
+    list_display = ['name', 'description']
+    search_fields = ['name']
+
+    # Customize other admin options as needed
 
 # Register models here
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
+
